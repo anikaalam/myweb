08963efcd1aa91660fffe185c73c462d87b613ec
anika <anika_4@live.com>
2020-08-05 06:35:23 -0400

    Updates

---
+++ precache-manifest.a3a541d7a44b9c57d8517ac34dbe6750.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306d4762a242618a82af4a5f7fd913e3",
+    "url": "/myweb/index.html"
+  },
+  {
+    "revision": "3266a5046057c1293ef2",
+    "url": "/myweb/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "95e3f50c5f12cba2a2da",
+    "url": "/myweb/static/css/main.178a59ae.chunk.css"
+  },
+  {
+    "revision": "3266a5046057c1293ef2",
+    "url": "/myweb/static/js/2.f446c1f4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myweb/static/js/2.f446c1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e3f50c5f12cba2a2da",
+    "url": "/myweb/static/js/main.c121526c.chunk.js"
+  },
+  {
+    "revision": "1e901c0afa5547618ddd",
+    "url": "/myweb/static/js/runtime-main.322427e1.js"
+  },
+  {
+    "revision": "21de06a30aaa4bedd08d8db6dd6f2725",
+    "url": "/myweb/static/media/profilepic.21de06a3.jpg"
+  },
+  {
+    "revision": "c924a2285bf1d762cb419dc751b3d952",
+    "url": "/myweb/static/media/resume.c924a228.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cde3adeda84fc47e7c230e2511bc544c.js"
+  "/myweb/precache-manifest.a3a541d7a44b9c57d8517ac34dbe6750.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myweb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
